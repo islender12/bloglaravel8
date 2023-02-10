397202bd7864d0877cf91a1a538ba7cc60a28da7
islender <70811036+islender12@users.noreply.github.com>
2023-02-10 13:00:11 -0400

    Mejora del Codigo

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Post;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PostRequest;
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -18,7 +20,7 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(): Renderable
     {
         $posts = Post::with('user:id,name')->get();
 
@@ -30,7 +32,7 @@ class PostController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create(): Renderable
     {
         return view('admin.create');
     }
@@ -41,43 +43,37 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(PostRequest $request)
+    public function store(PostRequest $request): RedirectResponse
     {
-        // Salvar
-        $post = Post::create([
-            'user_id' => auth()->user()->id
-        ] + $request->all());
+        try {
 
-        //imagen
+            // Salvar
+            $post = Post::create([
+                'user_id' => auth()->user()->id
+            ] + $request->all());
 
-        // if ($request->file('imagen')) {
-        //     $post->imagen = $request->file('imagen')->store('posts', 'public');
-        //     $post->save();
-        // }
+            //imagen
 
-        // Usamos paquete Intervention/image
+            // if ($request->file('imagen')) {
+            //     $post->imagen = $request->file('imagen')->store('posts', 'public');
+            //     $post->save();
+            // }
 
-        if ($request->file('imagen')) {
-            $ruta = storage_path('app\public/' . $request->file('imagen')->store('posts', 'public'));
-            $nombre = 'posts/' . basename($ruta);
-            Image::make($request->file('imagen'))->resize(500, 300)->save($ruta);
-            $post->imagen = $nombre;
-            $post->save();
+            // Usamos paquete Intervention/image
+
+            if ($request->file('imagen')) {
+                $ruta = storage_path('app\public/' . $request->file('imagen')->store('posts', 'public'));
+                $nombre = 'posts/' . basename($ruta);
+                Image::make($request->file('imagen'))->resize(500, 300)->save($ruta);
+                $post->imagen = $nombre;
+                $post->save();
+            }
+            // Retornar
+
+            return back()->with('status', 'Creado con Exito');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
         }
-        // Retornar
-
-        return back()->with('status', 'Creado con Exito');
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
     }
 
     /**
@@ -86,7 +82,7 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Post $post)
+    public function edit(Post $post): Renderable
     {
         return view('admin.edit', compact('post'));
     }
@@ -98,26 +94,29 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Post $post)
+    public function update(Request $request, Post $post): RedirectResponse
     {
-        //
-        $post->update($request->all());
+        try {
+            $post->update($request->all());
 
-        if ($request->file('imagen')) {
-            if ($post->imagen) {
-                Storage::disk('public')->delete($post->imagen);
-                $ruta = storage_path('app\public/' . $request->file('imagen')->store('posts', 'public'));
-                $nombre = 'posts/' . basename($ruta);
-                // con resize le damos un tamaño de 800x400
-                Image::make($request->file('imagen'))->resize(500, 300)->save($ruta);
+            if ($request->file('imagen')) {
+                if ($post->imagen) {
+                    Storage::disk('public')->delete($post->imagen);
+                    $ruta = storage_path('app\public/' . $request->file('imagen')->store('posts', 'public'));
+                    $nombre = 'posts/' . basename($ruta);
+                    // con resize le damos un tamaño de 800x400
+                    Image::make($request->file('imagen'))->resize(500, 300)->save($ruta);
 
-                $post->image = $nombre;
+                    $post->image = $nombre;
 
-                $post->save();
+                    $post->save();
+                }
             }
-        }
 
-        return redirect()->route('posts.edit', $post->slug)->with('status', 'Actualizado Correctamente');
+            return redirect()->route('posts.edit', $post->slug)->with('status', 'Actualizado Correctamente');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
     }
 
     /**
